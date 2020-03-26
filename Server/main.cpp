334b91e4d62b3538ef62b0fa55f03d7dc317c54e
Sp00nyMan <48148134+Sp00nyMan@users.noreply.github.com>
2020-03-26 17:53:46 +0300

    Server

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "Server.h"
 
-int main() {
-    //TODO
+int main(int argc, char** argv) {
+    if(argc != 2)
+    {
+        std::cerr << "Usage: server port\n";
+        return -1;
+    }
+    Server::initialize(atoi(argv[1]));
+    Server::start();
     return 0;
 }
