88a2336c263364ffa05b0dc6da4c4f40655310a6
Sp00nyMan <48148134+Sp00nyMan@users.noreply.github.com>
2020-03-26 16:07:06 +0300

    Changes

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,1 +1,40 @@
 #include "Client.h"
+
+int Client::send(const std::string &message) {
+    if (connect(socket, (sockaddr *) &serverAddress, sizeof(serverAddress)) < 0) {
+        std::cerr << "Error connecting to socket\n";
+        exit(-1);
+    }
+    int bytesSent = ::send(socket, message.c_str(), message.length(), 0);
+    close(socket);
+    return bytesSent;
+}
+
+std::string Client::recive()
+{
+    char buffer[255];
+    if(connect(socket, (sockaddr*)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        std::cerr << "Error connecting to socket\n";
+        exit(-1);
+    }
+    recv(socket, buffer, 255, 0);
+    close(socket);
+    return std::string(buffer);
+}
+
+Client::Client(const std::string &serverIP, int serverPort)
+{
+    bzero((char*)&serverAddress, sizeof(serverAddress));
+
+    serverAddress.sin_family = AF_INET;
+    struct hostent* host = gethostbyname(serverIP.c_str());
+    serverAddress.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr*)*host->h_addr_list));
+    serverAddress.sin_port = htons(serverPort);
+
+    socket = ::socket(serverAddress.sin_family, SOCK_STREAM, 0);
+    if(socket < 0) {
+        std::cerr << "Error establishing the server socket!\n";
+        exit(-1);
+    }
+}
